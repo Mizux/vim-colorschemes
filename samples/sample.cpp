4d7837eadceebe8f93c8d9fea9343cb5e420b8fe
Mizux Seiha <mizux.dev@gmail.com>
2020-04-20 23:54:58 +0200

    Update sample.cpp

--- samples/sample.cpp
+++ samples/sample.cpp
@@ -10,33 +10,27 @@
 #define SAMPLE_STR "This is a sample string"
 
 class Sample {
-  public:
-	/*! @brief Display a string.
-	 * longer description.
-	 * @param[in] input String to print.
-	 * @return nothing.*/
-	static void function(const std::string& input) {
-		do {
-			std::cout << input << __LINE__ << std::endl;
-		} while (false);
-		return;
-	}
-	static int getSample() {
-		return 1234;
+public:
+  /*! @brief Display a string.
+   * longer description.
+   * @param[in] input String to print.
+   * @return nothing.*/
+  static void function(const std::string &input) {
+    do {
+      std::cout << input << __LINE__ << std::endl;
+    } while (false);
+    return;
   }
-	static float getSampleFloat() {
-		return 12.34f;
-  }
-  virtual std::string getSampleString() const {
-    return SAMPLE_STR;
-  }
+  static int getSample() { return 1234; }
+  static float getSampleFloat() { return 12.34f; }
+  virtual std::string getSampleString() const { return SAMPLE_STR; }
 };
 
-int main(int argc, char** argv) {
-	for(std::size_t i=0; i < 1; ++i) {
-		// print Hello World !
-		Sample::function("Hello World !");
-	}
-	int a = 42;
-	return true ? 0.0f : 1;
+int main(int argc, char **argv) {
+  for (std::size_t i = 0; i < 1; ++i) {
+    // print Hello World !
+    Sample::function("Hello World !");
+  }
+  int a = 42;
+  return true ? 0.0f : 1;
 }
