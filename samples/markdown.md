881556cdd330fa6c34f8741f99c4584092888d42
Mizux Seiha <mizux.dev@gmail.com>
2025-09-24 01:06:35 +0200

    improve samples

--- samples/markdown.md
+++ samples/markdown.md
@@ -12,18 +12,27 @@ Lorem ipsum `dolor` sit amet, consectetur adipiscing elit. Sed non risus.
 ```code
 // comment
 foo = 42;
+bar = 42.10;
 ```
 
 # Title
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
 Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.
+1. lorem ipsum
+2. lorem ipsum
+3. lorem ipsum
 
 ## Title 2
 Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.
 Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.
 Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue.
+* lorem ipsum
+* lorem ipsum
+* lorem ipsum
 
 ### Title 3
 Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue.
 Praesent egestas leo in pede. Praesent blandit odio eu enim.
 Cras vestibulum bibendum augue. Praesent egestas leo in pede.
+
+[2]: url/to/load.webp
--- samples/sample.cpp
+++ samples/sample.cpp
@@ -1,26 +1,24 @@
 //! @file This is a doxygen comment
 //! @todo doxygen todo list
-// TODO generic todo
+// TODO(user) generic todo
+#include <exception>
 #include <iostream>
+#include <memory>
 #include <string>
 
-#ifdef NUMBER
-#define SAMPLE_INT 42
-#define SAMPLE_FLOAT 42.0f
-#define SAMPLE_DOUBLE 42.0
+#ifdef __LINUX__
+#define SAMPLE_VAL 42 42.0f 42.0
 #else
 #define SAMPLE_STR "This is a sample string"
 #endif
 
 namespace plop {
-  enum class Foo: std::uint8_t {
-    FOO = 0,
-    BAR = 1,
-  };
-  typedef Bar = enum class Foo;
-  using Bar = enum class Foo;
-}
-
+enum class Foo : std::uint8_t {
+  FOO = 0,
+  BAR = 1,
+};
+typedef Bar = enum class Foo;
+using Bar = enum class Foo;
 class Sample {
 public:
   /*! @brief Display a string.
@@ -28,23 +26,27 @@ public:
    * @param[in] input String to print.
    * @return nothing.*/
   static void function(const std::string &input) {
-    do {
-      std::cout << input << __LINE__ << std::endl;
-    } while (false);
+    try {
+      do {
+        std::cout << input << __LINE__ << "string literal" << std::endl;
+      } while (false);
+    } catch (std::exception &e) {
+    }
     return;
   }
   static int getSample() { return 1234; }
   static float getSampleFloat() { return 12.34f; }
   virtual std::string getSampleString() const { return SAMPLE_STR; }
 };
+} // namespace plop
 
 int main(int argc, char **argv) {
   for (std::size_t i = 0; i <= 1; ++i) {
     // print Hello World !
     if (i % 0x10)
-      Sample::function("Hello World !");
+      plop::Sample::function("Hello World !");
   }
-  Sample foo = new Sample();
+  std::unique_ptr<plop::Sample> foo = std::make_unique(new plop::Sample());
   int a = 42;
   return true ? 0.0f : 1;
 }
